9044d205f257a84b728516d245b413bb9d62cdf7
Warren Ste Wu <wuwarren7360@gmail.com>
2023-10-14 23:29:03 -0500

    Refactor HealthComponent and Extend Unit Tests
    Implemented a more structured and robust HealthComponent class with improved event handling for health changes. Refactored the OnHealthChanged event to utilize Action<int, int> delegate, encapsulating the old and new health values. Introduced additional methods Heal, SetMaxHealth, and SetCurrentHealth to manage health state. Enhanced the constructor to initialize MaxHealth and CurrentHealth properties. Expanded the unit tests to cover new functionality including healing, updating max health, and verifying death state. Ensured that the OnHealthChanged event is correctly triggered and handled in the tests, verifying the old and new health values.

--- Assets/Scripts/HealthComponent.cs
+++ Assets/Scripts/HealthComponent.cs
@@ -2,20 +2,61 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+using System;
+using UnityEngine;
+
 public class HealthComponent : MonoBehaviour
 {
-    //test
+    public event Action<int, int> OnHealthChanged;
+
     public int MaxHealth { get; private set; }
     public int CurrentHealth { get; private set; }
-
-    public HealthComponent(int maxHealth, int currentHealth)
-    {
-        MaxHealth = maxHealth;
-        CurrentHealth = currentHealth;
-    }
+    public bool IsDead { get; private set; }
 
     public void TakeDamage(int damage)
     {
+        int oldHealth = CurrentHealth;
         CurrentHealth = Mathf.Max(0, CurrentHealth - damage);
+        OnHealthChanged?.Invoke(oldHealth, CurrentHealth);
+        if (CurrentHealth <= 0)
+        {
+            IsDead = true;
+        }
+    }
+
+    public void Heal(int amount)
+    {
+        int oldHealth = CurrentHealth;
+        // If heal amount is greater than max health, set HP to max health
+        if ((CurrentHealth + amount) >= MaxHealth)
+        {
+            CurrentHealth = MaxHealth;
+        }
+        else
+        {
+            CurrentHealth += amount;
+        }
+        // Trigger the event if health changed
+        if (oldHealth != CurrentHealth)
+        {
+            OnHealthChanged?.Invoke(oldHealth, CurrentHealth);
+        }
+    }
+
+    public void SetMaxHealth(int amount)
+    {
+        MaxHealth = amount;
+    }
+
+    public void SetCurrentHealth(int amount)
+    {
+        int oldHealth = CurrentHealth;
+        CurrentHealth = amount;
+        // Trigger the event if health changed
+        if (oldHealth != CurrentHealth)
+        {
+            OnHealthChanged?.Invoke(oldHealth, CurrentHealth);
+        }
     }
 }
+
