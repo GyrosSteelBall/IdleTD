76b4b128b06f7e51fc4e696cc1717982f97cc2d4
GyrosSteelBall <glensimon16@gmail.com>
2023-10-15 23:59:44 -0500

    Added Pixel Art Top Down

---
+++ Assets/Scripts/HeroScriptableObjects.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "New Hero", menuName = "Hero")]
+public class HeroScriptableObject : ScriptableObject
+{
+    public string heroName;
+    public int attack, health, defense;
+    public float attackSpeed, range, critChance, critDamage;
+    public GameObject heroPrefab;
+    /*
+    public AttackComponent attackComponent;
+    public SpeedComponent speedComponent;
+    public DefenseComponent defenseComponent;
+    public HealthComponent healthComponent;
+    */
+
+
+
+
+
+
+
+
+}
