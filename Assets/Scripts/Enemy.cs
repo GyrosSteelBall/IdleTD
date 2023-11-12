aa5a60593d6bbac1f196ad379356fb47a4d7d22c
Warren Ste Wu <wuwarren7360@gmail.com>
2023-11-12 00:01:42 -0600

    Enemies blink when hit now

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -15,6 +16,13 @@ public class Enemy : MonoBehaviour
     private int currentWaypointIndex = 0;
     public int goldValue;
     private float distanceTraveled; // Total distance traveled along the path
+
+    [Header("Damage Feedback")]
+    [SerializeField] private SpriteRenderer enemySpriteRenderer;
+
+    private static Material defaultMaterial;
+    private static Material blinkMaterial;
+    private Coroutine damageCoroutine;
 
     // Property to access distance traveled
     public float DistanceAlongPath
@@ -65,10 +73,32 @@ public class Enemy : MonoBehaviour
     {
         health -= damage;
         healthBarSlider.value = health; // Update the slider value whenever the enemy takes damage
+        Blink();
         if (health <= 0)
         {
             Die();
         }
+    }
+
+    public void Blink()
+    {
+        if (defaultMaterial == null) defaultMaterial = enemySpriteRenderer.sharedMaterial;
+        if (blinkMaterial == null) blinkMaterial = new Material(Shader.Find("GUI/Text Shader"));
+
+        if (damageCoroutine != null)
+        {
+            StopCoroutine(damageCoroutine);
+        }
+        damageCoroutine = StartCoroutine(BlinkCoroutine());
+    }
+
+    private IEnumerator BlinkCoroutine()
+    {
+        enemySpriteRenderer.material = blinkMaterial;
+
+        yield return new WaitForSeconds(0.1f);
+
+        enemySpriteRenderer.material = defaultMaterial;
     }
 
     void Die()
