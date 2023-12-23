e54968000650f0ea0b0a81253e8205ba653a4b75
Warren Ste Wu <wuwarren7360@gmail.com>
2023-12-23 17:06:47 -0600

    Enemies continue moving when the unit in front of them dies now.

--- Assets/Scripts/refactor/EnemyManager/EnemyAttackingState.cs
+++ Assets/Scripts/refactor/EnemyManager/EnemyAttackingState.cs
@@ -3,6 +3,7 @@ public class EnemyAttackingState : IEnemyState
 {
     private UnitController target;
     private float nextAttackTime;
+    private EnemyController enemyController;
 
     public EnemyAttackingState(UnitController target)
     {
@@ -12,12 +13,21 @@ public class EnemyAttackingState : IEnemyState
 
     public void OnStateEnter(EnemyController enemyController)
     {
+        EventBus.Instance.Subscribe<UnitControllerDeathEvent>(HandleTargetDeath);
         EventBus.Instance.Publish(new EnemyControllerStateChangedEvent(enemyController, this));
+        this.enemyController = enemyController;
+    }
+
+    private void HandleTargetDeath(UnitControllerDeathEvent unitDeathEvent)
+    {
+        if (unitDeathEvent.UnitController == target)
+        {
+            this.enemyController.ChangeState(new EnemyMovingState());
+        }
     }
 
     public void Update(EnemyController enemyController)
     {
-        // Implement the logic for attacking the target
         if (Time.time >= nextAttackTime)
         {
             enemyController.Attack(target);
@@ -28,5 +38,6 @@ public class EnemyAttackingState : IEnemyState
     public void OnStateExit(EnemyController enemyController)
     {
         // Cleanup or final actions when leaving the attacking state
+        EventBus.Instance.Unsubscribe<UnitControllerDeathEvent>(HandleTargetDeath);
     }
 }
